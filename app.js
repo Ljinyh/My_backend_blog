6ae7a04681e69050c6fd44e4d759b98de7b9aecd
Ljinyh <com15643@naver.com>
2022-05-26 15:01:34 +0900

    index 수정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connect = require("./schemas");
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,7 @@ connect();
 const contentsRouter = require("./routes/contents");
 
 app.use(express.static("static"));
+app.use(bodyParser.urlencoded({extended:false}))
 
 const requestMiddleware = (req,res,next)=>{
     console.log("Request URL:", req.originalUrl, " - ", new Date());
@@ -20,9 +22,16 @@ app.use(requestMiddleware);
 
 app.use("/api", [contentsRouter]);
 
-app.get("/",(req, res) => {
-    res.send("Hello World");
+app.post("/contents", (req,res)=>{
+    res.render(__dirname + '/index.html')
+});
 
+app.get("/contents",(req, res) => {
+    res.render(__dirname + '/index.html')
+});
+
+app.get("/contents/detail", (req,res) => {
+    res.render(__dirname + '/detail.html')
 });
 
 app.listen(port, () => {
